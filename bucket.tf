a775ba6451de52a58c77a54930ecb9b62a360fe0
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-31 17:47:49 -0600

    bucket policy added

--- bucket.tf
+++ bucket.tf
@@ -7,3 +7,19 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+policy = <<POLICY
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::aws-account-id:root"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::bucket-name/prefix/*"
+    }
+  ]
+}
