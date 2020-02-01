57c2bb2f20202c3d9b658c3d75396c5add7f19e1
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-01 14:27:26 -0600

    added depends on

--- attachment_ASG_ELB.tf
+++ attachment_ASG_ELB.tf
@@ -1,4 +1,6 @@
 resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = "${var.asg_name}"
-  elb                    = "${aws_lb.test.id}"
+  autoscaling_group_name = "asg-dev"
+  elb                    = "test-lb-tf"
 }
+
+depends_on = [aws_module.asg]
