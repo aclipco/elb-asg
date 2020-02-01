11ac51e2a53161cd31b7ae80bbcd97ec9ad1b29e
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-01 14:52:20 -0600

    edit

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 
 output “this_autoscaling_group_id” {
-  description = “The autoscaling group id”
+  description = "The autoscaling group id"
   value       = "${aws_autoscaling_group.asg.id}"
 }
