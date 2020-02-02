1c2e75366d3fdc11b6efa9ae6f806d3bb1ba3b14
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-02 10:21:57 -0600

    edit

--- attachment_ASG_ELB.tf
+++ attachment_ASG_ELB.tf
@@ -2,6 +2,6 @@ resource "aws_autoscaling_attachment" "asg_attachment_bar" {
   autoscaling_group_name = "${module.asg.this_autoscaling_group_id}"
   elb                    = "test-lb-tf"
 }
-
+{
  depends_on = [aws_lb.test]
 }
